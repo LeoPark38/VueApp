e6101b2c22e24128403e9d45acc69f66949f10e1
DESKTOP-36BD3PV\Leo <packc38@gmail.com>
2023-03-28 21:11:06 +0900

    git 올리기 시작

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <the-header title="RememberMe"></the-header>
+  <stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+
+export default {
+  components: {
+    StoredResources,
+    TheHeader,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'guide',
+          title: '공식가이드',
+          description: 'Vue.js',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: '구글',
+          description: 'google...',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
